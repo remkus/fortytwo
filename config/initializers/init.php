e25ba75e6adb80141a52ae10bcfb00ad5bddaaa6
Remkus de Vries <remkus@forsite.nu>
2012-08-31 19:38:13 +0200

    Finally a repo just for me!

---
+++ config/initializers/init.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * FortyTwo Initializer File
+ */
+
+
+/** Load Initializer Components */
+require_once( 'genassist_enqueue_scripts.php' );
+require_once( 'genassist_preferences.php' );
+require_once( 'html5.php' );
+
+
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', 'FortyTwo' );
+define( 'CHILD_THEME_URL', 'http://forsitethemes.com' );
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/** Start the engine */
+require_once( get_template_directory() . '/lib/init.php' );
+
+/** Add FortyTwo Turbos */
+require_once( CHILD_DIR . '/config/initializers/init.php' );
