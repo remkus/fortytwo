4f39ddc9ec553a60570a3b676c2a756d670aa8a7
Ryan Holder <ryan@ryanholder.com>
2013-01-22 09:27:52 +0200

    commit content folder and config files

---
+++ wp-config-local.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'database_name_here' );
+define( 'DB_USER', 'username_here' );
+define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
