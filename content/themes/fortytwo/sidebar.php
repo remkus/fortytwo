6c4587cb5dbf69c2b0e406eaff8756c4d978ce74
Ryan Holder <ryan@ryanholder.com>
2013-02-01 10:27:14 +0200

    fortytwo theme

---
+++ content/themes/fortytwo/sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Handles primary sidebar structure in HTML5 markup.
+ *
+ * This file is a core Genesis file and should not be edited.
+ *
+ * @category Genesis
+ * @package  Templates
+ * @author   Forsite Themes
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ */
+
+?><section id="sidebar" class="sidebar widget-area" role="complementary">
+<?php
+	genesis_structural_wrap( 'sidebar' );
+	do_action( 'genesis_before_sidebar_widget_area' );
+	do_action( 'genesis_sidebar' );
+	do_action( 'genesis_after_sidebar_widget_area' );
+	genesis_structural_wrap( 'sidebar', 'close' );
+?>
+</section>
