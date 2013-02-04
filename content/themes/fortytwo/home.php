61e813337aba47d382205ddb6d8a451f328f8e8a
Ryan Holder <ryan@ryanholder.com>
2013-02-04 22:18:26 +0200

    Add widget areas to home page
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

---
+++ content/themes/fortytwo/home.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * FortyTwo Home Page
+ */
+
+add_action( 'genesis_meta', 'ft_home_genesis_meta' );
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ */
+function ft_home_genesis_meta() {
+
+	if ( is_active_sidebar( 'home-slider' ) || is_active_sidebar( 'home-notice' ) || is_active_sidebar( 'home-row-1-col-1' ) || is_active_sidebar( 'home-row-1-col-2' ) || is_active_sidebar( 'home-row-1-col-3' ) || is_active_sidebar( 'home-row-1-col-4' ) ) {
+
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+		add_action( 'genesis_after_header', 'fortytwo_home_slider_notice' );
+		add_action( 'genesis_loop', 'fortytwo_home_loop_helper' );
+		add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+		add_filter( 'body_class', 'add_body_class' );
+
+		function add_body_class( $classes ) {
+   			$classes[] = 'fortytwo';
+  			return $classes;
+		}
+
+	}
+}
+
+function fortytwo_home_slider_notice() {
+
+
+    if ( is_active_sidebar( 'home-slider' ) ) {
+        echo '<div id="home-slider">';
+        dynamic_sidebar( 'home-slider' );
+        echo '</div><!-- end #home-slider -->';
+    }
+
+    if ( is_active_sidebar( 'home-notice' ) ) {
+        echo '<div id="home-notice">';
+        dynamic_sidebar( 'home-notice' );
+        echo '</div><!-- end #home-notice -->';
+    }
+
+}
+
+function fortytwo_home_loop_helper() {
+
+	if ( is_active_sidebar( 'home-row-1-col-1' ) || is_active_sidebar( 'home-row-1-col-2' ) || is_active_sidebar( 'home-row-1-col-3' ) || is_active_sidebar( 'home-row-1-col-4' ) ) {
+
+		echo '<div id="home">';
+
+			echo '<div class="home-row-1-col-1">';
+			dynamic_sidebar( 'home-row-1-col-1' );
+			echo '</div><!-- end .home-row-1-col-1 -->';
+
+			echo '<div class="home-row-1-col-2">';
+			dynamic_sidebar( 'home-row-1-col-2' );
+			echo '</div><!-- end .home-row-1-col-2 -->';
+
+			echo '<div class="home-row-1-col-3">';
+			dynamic_sidebar( 'home-row-1-col-3' );
+			echo '</div><!-- end .home-row-1-col-3 -->';
+
+            echo '<div class="home-row-1-col-4">';
+            dynamic_sidebar( 'home-row-1-col-4' );
+            echo '</div><!-- end .home-row-1-col-4 -->';
+
+		echo '</div><!-- end #home -->';
+
+	}
+
+}
+
+genesis();
