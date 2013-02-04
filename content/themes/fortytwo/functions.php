fa29494dc0df6664c5de938e75cea263a059a35f
Ryan Holder <ryan@ryanholder.com>
2013-02-04 22:18:26 +0200

    Add widget areas to home page
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/functions.php
+++ content/themes/fortytwo/functions.php
@@ -1,13 +1,16 @@
 <?php
-/**
- * Functions file
- */
+/** Start the engine */
+require_once( TEMPLATEPATH . '/lib/init.php' );
+
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', 'FortyTwo Theme' );
+define( 'CHILD_THEME_URL', 'http://forsitethemes.com/themes/fortytwo' );
 
 add_theme_support( 'genesis-html5' );
 
 // Add Viewport meta tag for mobile browsers
-add_action( 'genesis_meta', 'ft_viewport_meta_tag' );
-function ft_viewport_meta_tag() {
+add_action( 'genesis_meta', 'fortytwo_viewport_meta_tag' );
+function fortytwo_viewport_meta_tag() {
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
 
@@ -24,8 +27,53 @@ function custom_load_custom_style_sheet() {
 }
 
 // Load Open Sans
-add_action( 'wp_enqueue_scripts', 'ft_load_google_fonts' );
-function ft_load_google_fonts() {
+add_action( 'wp_enqueue_scripts', 'fortytwo_load_google_fonts' );
+function fortytwo_load_google_fonts() {
 	wp_enqueue_style( 'google-font-open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,600,300', array(), PARENT_THEME_VERSION );
     wp_enqueue_style( 'google-font-droid-serif', 'http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic', array(), PARENT_THEME_VERSION );
 }
+
+// Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 4 );
+
+// Register widget areas
+genesis_register_sidebar( array(
+	'id'			=> 'home-slider',
+	'name'			=> __( 'Home Slider', 'fortytwo' ),
+	'description'	=> __( 'This is the slider section of the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+    'id'			=> 'home-notice',
+    'name'			=> __( 'Home Notice', 'fortytwo' ),
+    'description'	=> __( 'This is the notice section of the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-1-col-1',
+	'name'			=> __( 'Home Row 1 Column 1', 'fortytwo' ),
+	'description'	=> __( 'This is Column 1 of Row 1 on the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-1-col-2',
+	'name'			=> __( 'Home Row 1 Column 2', 'fortytwo' ),
+	'description'	=> __( 'This is Column 2 of Row 1 on the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-1-col-3',
+	'name'			=> __( 'Home Row 1 Column 3', 'fortytwo' ),
+	'description'	=> __( 'This is Column 3 of Row 1 on the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-1-col-4',
+	'name'			=> __( 'Home Row 1 Column 4', 'fortytwo' ),
+	'description'	=> __( 'This is Column 4 of Row 1 on the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-2-col-1',
+	'name'			=> __( 'Home Row 2 Column 1', 'fortytwo' ),
+	'description'	=> __( 'This is Column 1 of Row 1 on the homepage.', 'fortytwo' ),
+) );
+genesis_register_sidebar( array(
+	'id'			=> 'home-row-2-col-2',
+	'name'			=> __( 'Home Row 2 Column 2', 'fortytwo' ),
+    'description'	=> __( 'This is Column 2 of Row 2 on the homepage.', 'fortytwo' ),
+) );
