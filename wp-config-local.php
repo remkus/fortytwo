3925178c06b0d17e94b8d482a4503e05cf467982
Ryan Holder <ryan@ryanholder.com>
2013-02-21 11:34:27 +0200

    update local config settings

--- wp-config-local.php
+++ wp-config-local.php
@@ -8,5 +8,5 @@ You may include other settings here that you only want enabled on your local dev
 
 define( 'DB_NAME', 'forsitethemes_dev' );
 define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'w3bmonkey' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
