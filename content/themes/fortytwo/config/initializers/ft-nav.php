796fc3fca6a9441250003c9609e7f6c7b5e11701
Ryan Holder <ryan@ryanholder.com>
2013-02-22 10:29:12 +0200

    Add ft-scripts for adding in extra js and cs files
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/config/initializers/ft-nav.php
+++ content/themes/fortytwo/config/initializers/ft-nav.php
@@ -40,12 +40,12 @@ function fortytwo_genesis_do_nav() {
             $nav_output = <<<EOD
                 <nav class="primary navbar">
                     <div class="wrap container">
-                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                         </a>
-                        <div class="nav-collapse collapse">
+                        <div class="nav-collapse collapse navbar-responsive-collapse">
                           {$nav}
                         </div>
                     </div>
@@ -56,12 +56,12 @@ EOD;
 
             $nav_output = <<<EOD
                   <div id="nav" class="navbar">
-                      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                      <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                       </a>
-                      <div class="nav-collapse collapse">
+                      <div class="nav-collapse collapse navbar-responsive-collapse">
                           {$nav}
                       </div>
                   </div>
