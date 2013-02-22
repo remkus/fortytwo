182658320e883a1d77296dd68083539ed14aee5a
Ryan Holder <ryan@ryanholder.com>
2013-02-22 20:18:44 +0200

    Remove responsive slider inline styles
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/config/initializers/ft-cleanup.php
+++ content/themes/fortytwo/config/initializers/ft-cleanup.php
@@ -8,9 +8,20 @@
  */
 
 function fortytwo_cleanup_unwanted_styles() {
+
     // Removes the Genesis responsive slider stylesheet
     wp_dequeue_style( 'slider_styles' );
     wp_deregister_style( 'slider_styles' );
+
 }
 
 add_action( 'wp_print_styles', 'fortytwo_cleanup_unwanted_styles', 100 );
+
+// Remove the inline styles that Genesis Responsive slider adds
+function fortytwo_cleanup_inline_styles() {
+
+    remove_action( 'wp_head', 'genesis_responsive_slider_head', 1 );
+
+}
+
+add_action( 'init', 'fortytwo_cleanup_inline_styles', 100 );
