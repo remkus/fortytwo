d45e1b44a12e85815d6240ef3fa0400b8c3bc194
Ryan Holder <ryan@ryanholder.com>
2013-02-23 07:35:11 +0200

    Sort out merge conflict on ft-cleanup.php
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/config/initializers/ft-cleanup.php
+++ content/themes/fortytwo/config/initializers/ft-cleanup.php
@@ -7,9 +7,13 @@
  *
  */
 
+add_action( 'wp_print_styles', 'fortytwo_cleanup_unwanted_styles', 100 );
+/**
+ * Removes the Genesis responsive slider stylesheet
+ * @return [type] [description]
+ */
 function fortytwo_cleanup_unwanted_styles() {
 
-    // Removes the Genesis responsive slider stylesheet
     wp_dequeue_style( 'slider_styles' );
     wp_deregister_style( 'slider_styles' );
 
