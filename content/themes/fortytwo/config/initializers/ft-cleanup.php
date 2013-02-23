0855fc3b6db0f12665eac5073f311ffcca46a6d3
Ryan Holder <ryan@ryanholder.com>
2013-02-23 07:37:42 +0200

    Update ft-cleanup.php with new doc block format
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/config/initializers/ft-cleanup.php
+++ content/themes/fortytwo/config/initializers/ft-cleanup.php
@@ -19,13 +19,13 @@ function fortytwo_cleanup_unwanted_styles() {
 
 }
 
-add_action( 'wp_print_styles', 'fortytwo_cleanup_unwanted_styles', 100 );
-
-// Remove the inline styles that Genesis Responsive slider adds
+add_action( 'init', 'fortytwo_cleanup_inline_styles', 100 );
+/**
+ * Removes the Genesis responsive slider inline css block
+ * @return [type] [description]
+ */
 function fortytwo_cleanup_inline_styles() {
 
     remove_action( 'wp_head', 'genesis_responsive_slider_head', 1 );
 
 }
-
-add_action( 'init', 'fortytwo_cleanup_inline_styles', 100 );
