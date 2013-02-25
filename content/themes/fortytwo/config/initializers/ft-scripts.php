a1aabbf47839db92bd1f3dac42f24e0f99791c74
Ryan Holder <ryan@ryanholder.com>
2013-02-25 20:07:08 +0200

    Added Open Sans Condensed font
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/themes/fortytwo/config/initializers/ft-scripts.php
+++ content/themes/fortytwo/config/initializers/ft-scripts.php
@@ -14,6 +14,6 @@ add_action( 'wp_enqueue_scripts', 'fortytwo_add_scripts', 100 );
  */
 function fortytwo_add_scripts() {
 
-	wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/docs/assets/js/bootstrap.js', array( 'jquery' ), '3.0.0', true );
+    wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/docs/assets/js/bootstrap.js', array( 'jquery' ), '3.0.0', true );
 
 }
