be739329f103a0d4449e4a2b2a641bfed431fcbf
Ryan Holder <ryan@ryanholder.com>
2013-03-04 21:49:29 +0200

    Adding font awesome
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

---
+++ content/plugins/ft-featured-page/views/widget.php
@@ -0,0 +1,7 @@
+<!-- This file is used to markup the public-facing widget. -->
+<div class="post-x page type-page status-publish hentry entry">
+  <h3><i class="icon-beaker"></i> Services</h3>
+  <p>And purely one near this hey therefore darn firefly had ducked overpaid wow irrespective some tearful and mandrill
+    yikes considering far above. Physically less snickered much and and while</p>
+  <a class="btn" href="#">Our Services</a>
+</div>
