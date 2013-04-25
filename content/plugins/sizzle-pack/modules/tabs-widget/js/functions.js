95ef4b09ca91e5f4875eace59aac19679d5e2405
Ryan Holder <ryan@ryanholder.com>
2013-04-25 20:44:41 +0200

    Revert "Tabs widget using drop down select tab button"
    This reverts commit 1565758d9c36749d18649b0363b6f907936254db.

--- content/plugins/sizzle-pack/modules/tabs-widget/js/functions.js
+++ content/plugins/sizzle-pack/modules/tabs-widget/js/functions.js
@@ -1,8 +1,3 @@
 jQuery(function ($) {
-
-    $('.widget-ft-tabs .nav > .dropdown > a').html($('.widget-ft-tabs .nav .dropdown-menu > li.active').text() + ' <b class="caret"></b>');
-
-    $('.widget-ft-tabs .nav .dropdown-menu > li > a').on('click', function(){
-        $('.widget-ft-tabs .nav > .dropdown > a').html($(this).text() + ' <b class="caret"></b>');
-    });
+    $('.widget_fstpack_tabs a:first').tab('show');
 })
