16e6f687e2773c135ea80de9fb0e428f16631e7b
Ryan Holder <ryan@ryanholder.com>
2013-04-25 20:26:18 +0200

    Tabs widget using drop down select tab button
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- content/plugins/sizzle-pack/modules/tabs-widget/js/functions.js
+++ content/plugins/sizzle-pack/modules/tabs-widget/js/functions.js
@@ -1,3 +1,8 @@
 jQuery(function ($) {
-    $('.widget_fstpack_tabs a:first').tab('show');
+
+    $('.widget-ft-tabs .nav > .dropdown > a').html($('.widget-ft-tabs .nav .dropdown-menu > li.active').text() + ' <b class="caret"></b>');
+
+    $('.widget-ft-tabs .nav .dropdown-menu > li > a').on('click', function(){
+        $('.widget-ft-tabs .nav > .dropdown > a').html($(this).text() + ' <b class="caret"></b>');
+    });
 })
