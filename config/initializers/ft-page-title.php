c120a5829d97c276fdcde3f8d2fed2b8e5a8b786
Ryan Holder <ryan@ryanholder.com>
2013-05-23 22:47:23 +0100

    updates to FortyTwo to fix bugs in latest version of BS 3.0

---
+++ config/initializers/ft-page-title.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * FortyTwo Insert Page Title: Adds the page title to all pages
+ */
+
+
+add_action( 'genesis_after_header', 'fortytwo_insert_page_title' );
+/**
+ * Insert the Page Title on all pages except the home page
+ * @return [type] [description]
+ * @todo ft_page_title to be translated and possibly filterable
+ */
+function fortytwo_insert_page_title() {
+    if ( is_home() ) {
+        $ft_page_title = <<<EOD
+            <id class="page-title">
+                <section class="page-title-full">
+                    <h2>Page Title</h2>
+                </section>
+            </id>
+EOD;
+        echo $ft_page_title;
+    }
+}
