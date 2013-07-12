b5fdcaad4e9b25b89ce0e21fc601c5b6075181d2
Ryan Holder <ryan@ryanholder.com>
2013-07-12 11:46:20 +0100

    Fix incorrectly placed span in fortytwo_modify_archives_link function
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-default-widgets.php
+++ config/initializers/ft-default-widgets.php
@@ -21,17 +21,15 @@ function fortytwo_modify_archives_link( $link_html ) {
 
     $requested = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
-    if ( ! empty( $url )&& $requested == $url[1] ) {
-        $link_html = str_replace( '<li>', '<li class="archive-list-item current">', $link_html );
-    } else {
-        $link_html = str_replace( '<li>', '<li class="archive-list-item">', $link_html );
+    if ( ! empty( $url ) && strtolower( $requested ) == strtolower( $url[1] ) ) {
+        $link_html = str_replace( '<li>', '<li class="current-list-item">', $link_html );
     }
 
-    if ( empty( $post_count ) ) {
-        $link_html = str_replace( '</a>', '</a><span class="icon icon-angle-right"></span>', $link_html );
-    } else {
-        $link_html = str_replace( $post_count[0], '</span><span class="badge">' . $post_count[1] . '</span></a><span class="icon icon-angle-right">', $link_html );
+    if ( ! empty( $post_count ) ) {
+        $link_html = str_replace( $post_count[0], '<span class="badge">' . $post_count[1] . '</span></a>', $link_html );
     }
+
+    $link_html = str_replace( '</a>', '</a><span class="icon icon-angle-right"></span>', $link_html );
 
     return $link_html;
 
