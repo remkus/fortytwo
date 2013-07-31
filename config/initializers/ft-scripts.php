b3a0d21f3239590c4cd884b1c6b62164d04f2b01
Ryan Holder <ryan@ryanholder.com>
2013-07-31 22:24:23 +0100

    edit folder names under the vendor folder
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-scripts.php
+++ config/initializers/ft-scripts.php
@@ -15,5 +15,5 @@ add_action( 'wp_enqueue_scripts', 'fortytwo_add_scripts', 100 );
 function fortytwo_add_scripts() {
 
     wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/docs/assets/js/bootstrap.js', array( 'jquery' ), '3.0.0', true );
-    wp_enqueue_script( 'holderjs', CHILD_URL . '/vendor/javascripts/holder.js', array() , '1.9.0', true );
+    wp_enqueue_script( 'holderjs', CHILD_URL . '/vendor/js/holder.js', array() , '1.9.0', true );
 }
