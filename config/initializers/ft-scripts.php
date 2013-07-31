1cd3619d10fef427e35883be23be2aacbb472060
Ryan Holder <ryan@ryanholder.com>
2013-08-01 00:23:46 +0100

    1st draft of srcset for responsive images in place
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-scripts.php
+++ config/initializers/ft-scripts.php
@@ -14,6 +14,8 @@ add_action( 'wp_enqueue_scripts', 'fortytwo_add_scripts', 100 );
  */
 function fortytwo_add_scripts() {
 
-    wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/docs/assets/js/bootstrap.js', array( 'jquery' ), '3.0.0', true );
-    wp_enqueue_script( 'holderjs', CHILD_URL . '/vendor/js/holder.js', array() , '1.9.0', true );
+	wp_enqueue_script( 'srcset-polyfill', CHILD_URL . '/vendor/js/srcset.min.js', array() , '0.1.0', false );
+	wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/docs/assets/js/bootstrap.js', array( 'jquery' ), '3.0.0', true );
+	wp_enqueue_script( 'holderjs', CHILD_URL . '/vendor/js/holder.js', array() , '1.9.0', true );
+
 }
