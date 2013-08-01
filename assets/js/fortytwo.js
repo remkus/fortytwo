7e5d6e3c8caff75fc3093d59487d92dfb43b79f9
Ryan Holder <ryan@ryanholder.com>
2013-08-01 17:58:25 +0100

    Adding forty-two.js file
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

---
+++ assets/js/fortytwo.js
@@ -0,0 +1,9 @@
+/**
+ * Main fortytwo js file
+ */
+jQuery(document).ready(function($) {
+    // Inside of this function, $() will work as an alias for jQuery()
+    // and other libraries also using $ will not be accessible under this shortcut
+
+    
+});
