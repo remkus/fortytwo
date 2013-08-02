5b5aad016cf9480213c4c08bfd1974217832317a
David Laing <david@davidlaing.com>
2013-08-02 11:18:41 +0100

    Revert "Combine quoting styles"
    This reverts commit b92387e49a26a61c41fcc6d8acf26e6840c5929e.

--- lib/widgets/ft-testimonials/views/widget.php
+++ lib/widgets/ft-testimonials/views/widget.php
@@ -1,14 +1,10 @@
 <h4 class="widgettitle"><?php echo $instance['title']; ?></h4>
 <div class="testimonial-content">
   <?php foreach($instance['testimonials'] as $testimonial) { ?>
-  <figure>
-	  <blockquote>
-	    <p><?php echo $testimonial['content'];?></p>
-	    <figcaption>
-	    	<small>Someone famous <cite title="<?php echo $testimonial['title'];?>"><?php echo $testimonial['title'];?></cite></small>
-	    </figquote>
-	  </blockquote>
-  </figure>
+  <blockquote>
+    <p><?php echo $testimonial['content'];?></p>
+    <small>Someone famous <cite title="<?php echo $testimonial['title'];?>"><?php echo $testimonial['title'];?></cite></small>
+  </blockquote>
   <?php if ($testimonial !== end($instance['testimonials'])) { ?><hr /><?php } ?>
   <?php } ?>
 </div>
