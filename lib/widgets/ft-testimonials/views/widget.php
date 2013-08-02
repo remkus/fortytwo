3d6b2f5bbdaf63ed4c81f386e887f0832fa0fff3
David Laing <david@davidlaing.com>
2013-08-02 11:16:41 +0100

    Combine quoting styles

--- lib/widgets/ft-testimonials/views/widget.php
+++ lib/widgets/ft-testimonials/views/widget.php
@@ -1,10 +1,14 @@
 <h4 class="widgettitle"><?php echo $instance['title']; ?></h4>
 <div class="testimonial-content">
   <?php foreach($instance['testimonials'] as $testimonial) { ?>
-  <blockquote>
-    <p><?php echo $testimonial['content'];?></p>
-    <small>Someone famous <cite title="<?php echo $testimonial['title'];?>"><?php echo $testimonial['title'];?></cite></small>
-  </blockquote>
+  <figure>
+	  <blockquote>
+	    <p><?php echo $testimonial['content'];?></p>
+	    <figcaption>
+	    	<small>Someone famous <cite title="<?php echo $testimonial['title'];?>"><?php echo $testimonial['title'];?></cite></small>
+	    </figquote>
+	  </blockquote>
+  </figure>
   <?php if ($testimonial !== end($instance['testimonials'])) { ?><hr /><?php } ?>
   <?php } ?>
 </div>
