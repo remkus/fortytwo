1c1c9ba5d7e67a224ac497ac6f2ee9098b719efc
David Laing <david@davidlaing.com>
2013-08-02 12:01:24 +0100

    Output the quote author (if datasource supports it)

--- lib/widgets/ft-testimonials/views/widget.php
+++ lib/widgets/ft-testimonials/views/widget.php
@@ -3,7 +3,13 @@
   <?php foreach($instance['testimonials'] as $testimonial) { ?>
   <blockquote>
     <p><?php echo $testimonial['content'];?></p>
-    <small>Someone famous <cite title="<?php echo $testimonial['title'];?>"><?php echo $testimonial['title'];?></cite></small>
+    <small><?php if ($testimonial['quote_author']) { echo $testimonial['quote_author'].' '.__('in', 'fortytwo').' '; }?>
+    	<a href="<?php echo $testimonial['quote_source_link'];?>">
+    		<cite title="<?php echo $testimonial['quote_source'];?>">
+    			<?php echo $testimonial['quote_source'];?>
+    		</cite>
+    	</a>
+    </small>
   </blockquote>
   <?php if ($testimonial !== end($instance['testimonials'])) { ?><hr /><?php } ?>
   <?php } ?>
