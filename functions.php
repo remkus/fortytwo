a636bd3044190ac4c9ea34a8a695c71ea7e23a46
Ryan Holder <ryan@ryanholder.com>
2013-08-06 09:52:09 +0100

    A bunch of changes to start sorting out the layout issues with BS 3.0
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- functions.php
+++ functions.php
@@ -162,3 +162,12 @@ EOD;
  */
 remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
 add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
+
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	'menu-primary',
+	'menu-secondary',
+	'site-inner',
+	'footer-widgets',
+	'footer'
+) );
