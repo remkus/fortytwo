376d6956517444131b4196f6b11a50697c91bbac
Ryan Holder <ryan@ryanholder.com>
2013-08-07 17:39:06 +0100

    Some LESS cleanup and incorporating our grid changes based on .col-wrap
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-page-title.php
+++ config/initializers/ft-page-title.php
@@ -8,18 +8,18 @@ remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
 //add_action( 'genesis_after_header', 'genesis_do_breadcrumbs' ); //TODO bring in modified breadcrumbs
 
-add_action( 'genesis_after_header', 'fortytwo_insert_page_title' );
+add_action( 'genesis_after_header', 'fortytwo_insert_site_inner_header' );
 /**
- * Insert the Page Title on all pages except the home page
+ * Insert header section for site-inner headings
  * @return [type] [description]
- * @todo ft_page_title to be translated and possibly filterable
+ * @TODO $ft_site_inner_header to be translated and possibly filterable
  */
-function fortytwo_insert_page_title() {
+function fortytwo_insert_site_inner_header() {
     if ( !is_front_page() ) {
-        $ft_page_title = <<<EOD
-            <div class="site-inner-title">
+        $ft_site_inner_header = <<<EOD
+            <div class="site-inner-header">
                 <div class="wrap">
-                    <div class="col col-lg-6">
+                    <div class="col-wrap">
                         <h2>Page Title</h2>
                         <ul class="breadcrumb">
                             <li><a href="#">Home</a></li>
@@ -27,12 +27,9 @@ function fortytwo_insert_page_title() {
                             <li class="active">Data</li>
                         </ul>
                     </div>
-                    <div class="col col-lg-6">
-
-                    </div>
                 </div>
             </div>
 EOD;
-        echo $ft_page_title;
+        echo $ft_site_inner_header;
     }
 }
