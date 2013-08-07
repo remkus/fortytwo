8397bd6dd15a1ea7ac3db15b589bf8f8308363cc
Ryan Holder <ryan@ryanholder.com>
2013-08-08 00:38:09 +0100

    Updating the page title to reflect new thoughts on the structure, semantics and general naming of our added structure
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-page-title.php
+++ config/initializers/ft-page-title.php
@@ -8,28 +8,43 @@ remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
 //add_action( 'genesis_after_header', 'genesis_do_breadcrumbs' ); //TODO bring in modified breadcrumbs
 
-add_action( 'genesis_after_header', 'fortytwo_insert_site_inner_header' );
+add_action( 'genesis_after_header', 'fortytwo_insert_site_subheader' );
 /**
- * Insert header section for site-inner headings
+ * Insert subheader section for site-inner
+ *
  * @return [type] [description]
- * @TODO $ft_site_inner_header to be translated and possibly filterable
+ * @TODO $ft_site_subheader to be translated and possibly filterable
  */
-function fortytwo_insert_site_inner_header() {
-    if ( !is_front_page() ) {
-        $ft_site_inner_header = <<<EOD
-            <div class="site-inner-header">
-                <div class="wrap">
-                    <div class="col-wrap">
-                        <h2>Page Title</h2>
-                        <ul class="breadcrumb">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Library</a></li>
-                            <li class="active">Data</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+function fortytwo_insert_site_subheader() {
+	if ( !is_front_page() ) {
+		$ft_site_subheader = <<<EOD
+			<div class="site-subheader">
+				<div class="outer-wrap">
+					<div class="wrap">
+						<div class="subheader-area">
+							<h2>Page Title</h2>
+							<ul class="breadcrumb">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Library</a></li>
+								<li class="active">Data</li>
+							</ul>
+						</div>
+						<aside class="widget-area subheader-widget-area">
+							<section id="search-4" class="widget widget_search">
+								<div class="widget-wrap">
+									<form role="search" method="get" id="searchform" class="input-group" action="http://www.gitpress.dev/">
+										<input type="text" value="" name="s" id="s">
+										<span class="input-group-btn">
+											<button class="btn btn-default" type="submit">Search</button>
+										</span>
+									</form>
+								</div>
+							</section>
+						</aside>
+					</div>
+				</div>
+			</div>
 EOD;
-        echo $ft_site_inner_header;
-    }
+		echo $ft_site_subheader;
+	}
 }
