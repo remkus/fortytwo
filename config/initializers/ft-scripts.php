84ef65def5500d75146ee0a1599cd1c35e1bc36e
Ryan Holder <ryan@ryanholder.com>
2013-08-09 15:16:10 +0100

    Some minor file cleanup
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-scripts.php
+++ config/initializers/ft-scripts.php
@@ -7,7 +7,7 @@
 
 add_action( 'wp_enqueue_scripts', 'fortytwo_add_scripts', 100 );
 /**
- * Load fortytwo stylesheet to header
+ * Load fortytwo required scripts
  * @return [type] [description]
  * @todo  These needs to be named bootstrap to prevent FST-PACK from also loading bootstrap
  * @todo  Need to change bootstrap.js for final version to minified
@@ -17,8 +17,10 @@ function fortytwo_add_scripts() {
 	// Adding the fortytwo.js file
 	wp_enqueue_script( 'fortytwo', CHILD_URL . '/assets/js/fortytwo.js', array() , '0.1.0', false );
 
+	// Polyfill for srcset which is part of our responsive images solution
 	wp_enqueue_script( 'srcset-polyfill', CHILD_URL . '/vendor/js/srcset.min.js', array() , '0.1.0', false );
+
+	// Bootstrap js file
 	wp_enqueue_script( 'bootstrap', CHILD_URL . '/vendor/frameworks/bootstrap/dist/js/bootstrap.min.js', array( 'jquery' ), '3.0.0', true );
-	wp_enqueue_script( 'holderjs', CHILD_URL . '/vendor/js/holder.js', array() , '1.9.0', true );
 
 }
