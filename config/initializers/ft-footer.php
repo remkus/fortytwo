ee76c03efbd81f922bf7f125874bb73d8efb268e
Ryan Holder <ryan@ryanholder.com>
2013-08-09 17:59:27 +0100

    Adding a single footer-widgets area and bypassing the Genesis function that creates them
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

---
+++ config/initializers/ft-footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * FortyTwo Footer Widget Areas
+ *
+ * @package FortyTwo
+ */
+
+add_action( 'genesis_before_footer', 'fortytwo_insert_footer_widget' );
+/**
+ * Echo the markup necessary to facilitate the footer widget area.
+ *
+ * We are creating one widget area and columns are determined by no. widgets
+ *
+ */
+function fortytwo_insert_footer_widget() {
+
+	$footer_widgets = get_theme_support( 'genesis-footer-widgets' );
+
+	if ( ! $footer_widgets )
+		return;
+
+	//* Check to see if footer-columns has widgets.
+	if ( ! is_active_sidebar( 'footer-columns' ) )
+		return;
+
+	genesis_markup( array(
+		'html5'   => '<div %s>',
+		'xhtml'   => '<div id="footer-widgets" class="footer-widgets">',
+		'context' => 'footer-widgets'
+	) );
+
+	genesis_structural_wrap( 'footer-widgets', 'open' );
+
+	dynamic_sidebar( 'footer-columns' );
+
+	genesis_structural_wrap( 'footer-widgets', 'close' );
+
+	echo '</div>';
+
+}
+
+
+
