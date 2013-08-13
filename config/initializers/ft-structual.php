cff58c7986d2874e9fe7f74e4358e04f8f716287
Ryan Holder <ryan@ryanholder.com>
2013-08-13 13:30:55 +0100

    site-inner needs to use the same functions for our extra structural wrap and therefore we need to add site-inner to structural-wrap theme support
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-structual.php
+++ config/initializers/ft-structual.php
@@ -7,8 +7,17 @@
  *
  */
 
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	'menu-primary',
+	'menu-secondary',
+	'site-inner',
+	'footer-widgets',
+	'footer'
+) );
+
 add_filter( 'genesis_structural_wrap-header', 'fortytwo_add_extra_structural_wrap', 10, 2 );
-//add_filter( 'genesis_structural_wrap-site-inner', 'fortytwo_add_extra_structural_wrap', 10, 2 );
+add_filter( 'genesis_structural_wrap-site-inner', 'fortytwo_add_extra_structural_wrap', 10, 2 );
 add_filter( 'genesis_structural_wrap-footer-widgets', 'fortytwo_add_extra_structural_wrap', 10, 2 );
 add_filter( 'genesis_structural_wrap-footer', 'fortytwo_add_extra_structural_wrap', 10, 2 );
 /**
@@ -53,44 +62,6 @@ function fortytwo_attributes_extra_structural_wrap( $attributes ) {
 
 	return $attributes;
 
-}
-
-add_action( 'genesis_before_content_sidebar_wrap', 'fortytwo_need_name_insert_open_outer_wrap' );
-/**
- * Add a structural extra .wrap div so we can rename the original one later in the
- * process of adding structural wraps.
- *
- * @since 1.0
- *
- * @param string $output Existing attributes.
- * @param string $output_original
- *
- * @return string Wrap HTML with col-wrap.
- */
-function fortytwo_need_name_insert_open_outer_wrap() {
-
-	$output = sprintf( '<div %s>', genesis_attr( 'extra-structural-wrap' ) );
-
-	echo $output;
-}
-
-add_action( 'genesis_after_content_sidebar_wrap', 'fortytwo_need_name_insert_close_outer_wrap' );
-/**
- * Add a structural extra .wrap div so we can rename the original one later in the
- * process of adding structural wraps.
- *
- * @since 1.0
- *
- * @param string $output Existing attributes.
- * @param string $output_original
- *
- * @return string Wrap HTML with col-wrap.
- */
-function fortytwo_need_name_insert_close_outer_wrap() {
-
-	$output = '</div>';
-
-	echo $output;
 }
 
 /**
