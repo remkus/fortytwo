8a3ed5c51097a0ce80a0bcf0a2758a08e7185fb3
Ryan Holder <ryan@ryanholder.com>
2013-08-20 17:28:01 +0100

    Making changes to the FT Tabs widget including name changes to FT Tabbed Content
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- lib/widgets/ft-testimonials/views/widget.php
+++ lib/widgets/ft-testimonials/views/widget.php
@@ -1,10 +1,11 @@
 <h4 class="widgettitle"><?php echo $instance['title']; ?></h4>
 <div class="testimonial-content">
-  <?php foreach($instance['testimonials'] as $testimonial) { ?>
-  <blockquote>
-    <p><?php echo $testimonial['content'];?></p>
-    <small><?php echo $testimonial['quote_source_formatted'];?></small>
-  </blockquote>
-  <?php if ($testimonial !== end($instance['testimonials'])) { ?><hr /><?php } ?>
-  <?php } ?>
+	<?php foreach ( $instance['testimonials'] as $testimonial ) { ?>
+		<blockquote>
+			<p><?php echo $testimonial['content']; ?></p>
+			<small><?php echo $testimonial['quote_source_formatted']; ?></small>
+		</blockquote>
+		<?php if ( $testimonial !== end( $instance['testimonials'] ) ) { ?>
+			<hr/><?php } ?>
+	<?php } ?>
 </div>
