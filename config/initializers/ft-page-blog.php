3f0c13974e1669aee4e64ca2855bd3ae1e8989f1
Ryan Holder <ryan@ryanholder.com>
2013-08-27 12:54:13 +0100

    Blog page image post spacing
    Signed-off-by: Ryan Holder <ryan@ryanholder.com>

--- config/initializers/ft-page-blog.php
+++ config/initializers/ft-page-blog.php
@@ -6,29 +6,6 @@
  *
  */
 
-//* HTML5 Hooks
-// add_action( 'genesis_entry_header', 'genesis_do_post_format_image', 5 );
-// add_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-// add_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-// add_action( 'genesis_entry_header', 'genesis_do_post_title' );
-
-// add_action( 'genesis_entry_content', 'genesis_do_post_image' );
-// add_action( 'genesis_entry_content', 'genesis_do_post_content' );
-// add_action( 'genesis_entry_content', 'genesis_do_post_permalink' );
-// add_action( 'genesis_entry_content', 'genesis_do_post_content_nav' );
-
-// add_action( 'genesis_entry_header', 'genesis_post_info' );
-
-// add_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-// add_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
-// add_action( 'genesis_entry_footer', 'genesis_post_meta' );
-
-// add_action( 'genesis_after_entry', 'genesis_do_author_box_single' );
-
-//* Other
-// add_action( 'genesis_loop_else', 'genesis_do_noposts' );
-// add_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
-
 // Modify the blog page template, new FortyTwo layout
 
 remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
@@ -51,7 +28,7 @@ function fortytwo_do_post_image() {
 		) );
 
 		if ( !empty( $img ) )
-			printf( '<a href="%s" title="%s" class="pull-left">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), $img );
+			printf( '<a href="%s" title="%s" class="post-image-link">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), $img );
 	}
 
 }
