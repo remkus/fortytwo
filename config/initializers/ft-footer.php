8e7db115679dc21b05ae476e646415d3cacf2ec3
Daan Kortenbach <daan@kortenbach.org>
2013-09-16 17:08:39 +0200

    move footer stuff from functions.php to ft-footer.php
    Signed-off-by: Daan Kortenbach <daan@kortenbach.org>

--- config/initializers/ft-footer.php
+++ config/initializers/ft-footer.php
@@ -39,3 +39,21 @@ function fortytwo_insert_footer_widget() {
 	echo '</div>';
 
 }
+
+
+/** Customize the default footer */
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+
+add_action( 'genesis_footer', 'fortytwo_custom_footer' );
+
+function fortytwo_custom_footer() {
+
+	$footer_output = <<<EOD
+		<div class="copyright-area">
+			<span>&copy; Copyright 2012 <a href="http://mydomain.com/">My Domain</a> &middot; All Rights Reserved &middot; Powered by <a href="http://wordpress.org/">WordPress</a> &middot; <a href="http://mydomain.com/wp-admin">Admin</a></span>
+		</div>
+EOD;
+
+	echo $footer_output;
+
+}
--- functions.php
+++ functions.php
@@ -13,21 +13,3 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 /** Add FortyTwo Turbos */
 require_once 'config/initializers/init.php';
-
-
-/** Customize the default footer */
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-
-add_action( 'genesis_footer', 'fortytwo_custom_footer' );
-
-function fortytwo_custom_footer() {
-
-	$footer_output = <<<EOD
-		<div class="copyright-area">
-			<span>&copy; Copyright 2012 <a href="http://mydomain.com/">My Domain</a> &middot; All Rights Reserved &middot; Powered by <a href="http://wordpress.org/">WordPress</a> &middot; <a href="http://mydomain.com/wp-admin">Admin</a></span>
-		</div>
-EOD;
-
-	echo $footer_output;
-
-}
