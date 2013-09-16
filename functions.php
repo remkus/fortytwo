43112251dba9f2b5fb72557eadddab0eefe988a6
Daan Kortenbach <daan@kortenbach.org>
2013-09-16 18:27:33 +0200

    Add theme support for HTML5 with Semantic Markup
    Signed-off-by: Daan Kortenbach <daan@kortenbach.org>

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ require_once TEMPLATEPATH . '/lib/init.php';
 /** Localization */
 load_child_theme_textdomain( 'fortytwo', CHILD_DIR . '/lib/languages' );
 
-/** Activates html5 support */
-add_theme_support( 'html5' );
+/** Add theme support for HTML5 with Semantic Markup */
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 
 /** Adds responsive viewport */
 add_theme_support( 'genesis-responsive-viewport' );
