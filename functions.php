664a9dcf8e0e2d9f9375d725a9da5d9cf3b80c0b
Daan Kortenbach <daan@kortenbach.org>
2013-09-16 17:07:37 +0200

    move theme support before FortyTwo init
    Signed-off-by: Daan Kortenbach <daan@kortenbach.org>

--- functions.php
+++ functions.php
@@ -5,14 +5,15 @@ require_once TEMPLATEPATH . '/lib/init.php';
 /** Localization */
 load_child_theme_textdomain( 'fortytwo', get_stylesheet_directory() . '/lib/languages' );
 
+/** Activates html5 support */
+add_theme_support( 'html5' );
+
+/** Adds responsive viewport */
+add_theme_support( 'genesis-responsive-viewport' );
+
 /** Add FortyTwo Turbos */
 require_once 'config/initializers/init.php';
 
-/**
- * Activates FortyTwo theme features.
- */
-add_theme_support( 'html5' );
-add_theme_support( 'genesis-responsive-viewport' );
 
 /** Customize the default footer */
 remove_action( 'genesis_footer', 'genesis_do_footer' );
