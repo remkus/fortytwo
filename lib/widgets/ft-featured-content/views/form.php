19842c8c7f9aa57290200d0c654c262b9fa8f982
Remkus de Vries <remkus@forsite.nu>
2013-12-19 14:55:02 +0100

    Various CSS changes to the widgets. Fixes #136

--- lib/widgets/ft-featured-content/views/form.php
+++ lib/widgets/ft-featured-content/views/form.php
@@ -6,7 +6,7 @@
 
 <div class="ft-featured-content-admin">
 <input type="hidden" <?php $this->echo_field_id( "icon" ) ?> value="<?php echo esc_attr( $instance['icon'] ); ?>">
-<table>
+<table style="width: 100%">
 	<tr>
 		<td class="<?php echo $this->get_field_id( "the-icon-selector" )?>">
 				<div >
@@ -14,20 +14,20 @@
 				</div>
 		</td>
 		<td>
-			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Title', 'fortytwo' ); ?></label>
-			<input class="span2" type="text" <?php $this->echo_field_id( "title" ) ?>  value="<?php echo esc_attr( $instance['title'] ); ?>">
+			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Title:', 'fortytwo' ); ?></label>
+			<input class="widefat" type="text" <?php $this->echo_field_id( "title" ) ?>  value="<?php echo esc_attr( $instance['title'] ); ?>">
 		</td>
   </tr>
 	<tr><td colspan="2">
-			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Content', 'fortytwo' ); ?></label>
-		  <textarea rows="5" <?php $this->echo_field_id( "content" ) ?>
+			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Content:', 'fortytwo' ); ?></label>
+		  <textarea class="widefat" <?php $this->echo_field_id( "content" ) ?>
 		  	><?php echo esc_textarea( $instance['content'] ); ?></textarea>
   </td></tr>
 	<tr><td colspan="2">
-			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Button text', 'fortytwo' ); ?></label>
-			<input type="text" <?php $this->echo_field_id( "button_text" ) ?> value="<?php echo esc_attr( $instance['button_text'] ); ?>"><br />
-		  <label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Button link', 'fortytwo' ); ?></label>
-		  <input type="text" <?php $this->echo_field_id( "button_link" ) ?> value="<?php echo esc_attr( $instance['button_link'] ); ?>">
+			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Button Text:', 'fortytwo' ); ?></label>
+			<input class="widefat" type="text" <?php $this->echo_field_id( "button_text" ) ?> value="<?php echo esc_attr( $instance['button_text'] ); ?>"><br />
+		  <label for="<?php echo $this->get_field_id( 'button_text' ); ?>"><?php _e( 'Button Link:', 'fortytwo' ); ?></label>
+		  <input class="widefat" type="text" <?php $this->echo_field_id( "button_link" ) ?> value="<?php echo esc_attr( $instance['button_link'] ); ?>">
 		</td>
 	</tr>
 </table>
--- lib/widgets/ft-testimonials/views/form.php
+++ lib/widgets/ft-testimonials/views/form.php
@@ -1,31 +1,31 @@
 <div class="ft-testimonials-admin">
-<table>
+<table style="width:100%">
 	<tr>
 		<td>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'fortytwo' ); ?></label>
-			<input class="span2" type="text" <?php $this->echo_field_id( "title" ) ?> value="<?php echo esc_attr( $instance['title'] ); ?>">
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'fortytwo' ); ?></label>
+			<input class="widefat" type="text" <?php $this->echo_field_id( "title" ) ?> value="<?php echo esc_attr( $instance['title'] ); ?>">
 		</td>
   </tr>
 	<tr>
 		<td>
-			<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e( 'Limit', 'fortytwo' ); ?></label>
-			<input class="span2" type="text" <?php $this->echo_field_id( "limit" ) ?> value="<?php echo esc_attr( $instance['limit'] ); ?>">
+			<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e( 'Limit:', 'fortytwo' ); ?></label>
+			<input class="widefat" type="text" <?php $this->echo_field_id( "limit" ) ?> value="<?php echo esc_attr( $instance['limit'] ); ?>">
 		</td>
   </tr>
 	<tr>
 		<td>
-			<label for="<?php echo $this->get_field_id( 'datasource' ); ?>"><?php _e( 'Datasource', 'fortytwo' ); ?></label> 
-			<select <?php $this->echo_field_id( 'datasource' ) ?> class="widefat">
-				<option <?php if ( '' == $instance['datasource'] ) echo 'selected="selected"'; ?>>==&gt;&nbsp;<?php _e( 'Select', 'fortytwo' ); ?>&nbsp;&lt;==</option>
+			<label for="<?php echo $this->get_field_id( 'datasource' ); ?>"><?php _e( 'Datasource:', 'fortytwo' ); ?></label>
+			<select class="widefat" <?php $this->echo_field_id( 'datasource' ) ?> >
+				<option class="widefat" <?php if ( '' == $instance['datasource'] ) echo 'selected="selected"'; ?>>==&gt;&nbsp;<?php _e( 'Select', 'fortytwo' ); ?>&nbsp;&lt;==</option>
 				<?php foreach ($instance['datasources'] as $datasource) { ?>
-				<option value="<?php echo $datasource['value']; ?>" <?php if ( $datasource['value'] == $instance['datasource'] ) echo 'selected="selected"'; ?>>
+				<option class="widefat" value="<?php echo $datasource['value']; ?>" <?php if ( $datasource['value'] == $instance['datasource'] ) echo 'selected="selected"'; ?>>
 					<?php _e( $datasource['name'], 'fortytwo' ); ?>
 				</option>
 				<?php } ?>
 			</select>
 			<div class="<?php echo $this->get_field_id( 'datasource' ) ?>">
 				<div class="category datasource_block">
-					<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php _e( 'Category', 'fortytwo' ); ?></label> 
+					<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php _e( 'Category', 'fortytwo' ); ?></label>
 					<?php wp_dropdown_categories(array(
 						'id'=>$this->get_field_id( 'category' ),
 						'name'=>$this->get_field_name( 'category' ),
@@ -59,7 +59,7 @@
 	"use strict";
 	$(document).ready(function () {
 		var display_active_datasource = function(active_datasource) {
-			$('.<?php echo $this->get_field_id( "datasource" ); ?> .datasource_block').each( function() {	
+			$('.<?php echo $this->get_field_id( "datasource" ); ?> .datasource_block').each( function() {
 					if (this.className.indexOf(active_datasource)>=0) {
 						$(this).addClass('make_visible');
 					} else {
@@ -67,7 +67,7 @@
 					}
 			});
 		}
-		$('#<?php echo $this->get_field_id( "datasource" )?>').change(function() 
+		$('#<?php echo $this->get_field_id( "datasource" )?>').change(function()
 		{
 			var active_datasource = $(this).attr('value');
 			display_active_datasource(active_datasource);
