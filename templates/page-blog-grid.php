d87ca3d7269d6f9a47ea26457d1b75a146bfcc8b
Remkus de Vries <remkus@forsite.nu>
2014-01-29 15:53:50 +0100

    Committing the page templates to this branch. This I won’t close the feature branch :)

---
+++ templates/page-blog-grid.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Blog Grid
+ * The file handles the grid blog layout of FortyTwo.
+ *
+ * @category FortyTwo
+ * @package  Templates
+ * @author   Forsite Themes
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     http://www.forsitethemes.com/
+ */
+
