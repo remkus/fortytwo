2f639ee3ecacd180753176480c2cdeb14980ccc5
Gary Jones <gary@garyjones.co.uk>
2014-05-14 22:59:29 +0100

    Pass JSHint for code standards.
    A few code standards issue remain, regarding whitespace,
    but JSHint task now runs without errors.
    
    Fixes #198

--- assets/js/fortytwo.js
+++ assets/js/fortytwo.js
@@ -1,22 +1,17 @@
 /**
  * Main fortytwo js file
  */
-jQuery(document ).ready( function ($) {
-	// Inside of this function, $() will work as an alias for jQuery()
-	// and other libraries also using $ will not be accessible under this shortcut
-
+(function ($) {
+	'use strict';
 	setTimeout( function () {
-		var $navbar = $( '.nav-primary' )
+		var $navbar = $( '.nav-primary' );
 
 		$navbar.affix( {
 			offset: {
 				top: function () {
-					return ( this.top = $(".site-header").outerHeight(true) )
+					return ( this.top = $( '.site-header' ).outerHeight(true) );
 				}
 			}
-		} )
-
-
-	}, 100 )
-
-} );
+		} );
+	}, 100 );
+}( jQuery ) );
