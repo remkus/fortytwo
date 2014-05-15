bd8d550388b2353bb35592993f5e9b41f48684aa
Gary Jones <gary@garyjones.co.uk>
2014-05-15 13:50:13 +0100

    Hide archive and blog page templates
    These templates are inherited from Genesis. The blog template is advised
    not to use and the archive page isn't currently expected to be used.
    
    Unit tests for page templates removal included. Test to check Business
    page template is present is also included.
    
    Fixes #199

---
+++ lib/admin/ft-templates.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * FortyTwo Theme.
+ *
+ * @package FortyTwo\Admin
+ * @author  Forsite Themes
+ * @license GPL-2.0+
+ * @link    http://forsitethemes/themes/fortytwo/
+ */
+
+add_filter( 'theme_page_templates', 'ft_remove_page_templates' );
+/**
+ * Remove archive and blog page templates inherited from Genesis.
+ *
+ * This hides them from the Page Template dropdowns.
+ *
+ * @param array $templates Existing templates filenames.
+ *
+ * @return array Amended template filenames.
+ */
+function ft_remove_page_templates( $templates ) {
+	unset( $templates['page_blog.php'] );
+	unset( $templates['page_archive.php'] );
+
+	return $templates;
+}
--- lib/init.php
+++ lib/init.php
@@ -27,6 +27,7 @@ define( 'FORTYTWO_WIDGETS_URL', FORTYTWO_LIB_URL . '/widgets' );
 /* Load Initializer Components */
 require_once 'admin/ft-admin-style.php';
 require_once 'admin/ft-layouts.php';
+require_once 'admin/ft-templates.php';
 
 require_once 'general/ft-default-widgets.php';
 require_once 'general/ft-scripts.php';
