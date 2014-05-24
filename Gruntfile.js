74e400b9fdec92de27d16e03c1e02124007d2d90
Gary Jones <gary@garyjones.co.uk>
2014-05-25 00:44:37 +0100

    Improve LESS comments for better styledocco output
    Use markdown headers, avoid bullet points etc.
    
    Also removes numbered sections. Child themes
    forked from FortyTwo may rearrange sections, or
    add or remove complete sections, which makes the
    fixing up of numbered sections extra maintenance.
    There are no style.css search benefits for having
    numbers since the table of contents already show
    the names of sections.
    
    style.css updated with the latest build that
    includes the new markdown comments and new replace
    task.
    
    Fixes #209

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,18 +199,18 @@ module.exports = function(grunt) {
 			},
 			style: {
 				options: {
-					banner: '/*!\n' +
-						'  Theme Name: <%= pkg.theme.name %>\n' +
-						'  Theme URI: <%= pkg.theme.uri %>\n' +
-						'  Description: <%= pkg.theme.description %>\n' +
-						'  Author: <%= pkg.theme.author %>\n' +
-						'  Author URI: <%= pkg.theme.authoruri %>\n' +
-						'  Version: <%= pkg.version %>\n' +
-						'  Tags: <%= pkg.theme.tags %>\n' +
-						'  Text Domain: <%= pkg.theme.textdomain %>\n\n' +
-						'  License: <%= pkg.theme.license %>\n' +
-						'  License URI: <%= pkg.theme.licenseuri %>\n\n' +
-						'  Template: <%= pkg.theme.template %>\n' +
+					banner: '/* # FortyTwo\n' +
+						' * Theme Name: <%= pkg.theme.name %>\n' +
+						' * Theme URI: <%= pkg.theme.uri %>\n' +
+						' * Description: <%= pkg.theme.description %>\n' +
+						' * Author: <%= pkg.theme.author %>\n' +
+						' * Author URI: <%= pkg.theme.authoruri %>\n' +
+						' * Version: <%= pkg.version %>\n' +
+						' * Tags: <%= pkg.theme.tags %>\n' +
+						' * Text Domain: <%= pkg.theme.textdomain %>\n' +
+						' * License: <%= pkg.theme.license %>\n' +
+						' * License URI: <%= pkg.theme.licenseuri %>\n' +
+						' * Template: <%= pkg.theme.template %>\n' +
 						'*/\n\n'
 				},
 				src: [
