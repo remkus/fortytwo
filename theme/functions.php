dffa8369a8d8e0943048dbefdccd5cad7bb18054
Gary Jones <gary@garyjones.co.uk>
2014-05-25 11:38:25 +0100

    Dynamically require widget files
    Moves the data for which widget files are loaded into functions.php
    and out of the lib/ directory.
    
    New widgets can be added by dropping the files into subdirectory of
    lib/widgets, then adding support for it in functions.php.
    
    Also means that base widget file is only required if any widgets are supported, and wpthumb library is only required if responsive slider widget is supported.

--- theme/functions.php
+++ theme/functions.php
@@ -8,20 +8,37 @@
  * @link    http://forsitethemes/themes/fortytwo/
  */
 
-/**
- * Localization
- */
+// Load translation files
 load_child_theme_textdomain( 'fortytwo', get_stylesheet_directory() . '/lib/languages' );
 
-/**
- * Add theme support for HTML5 with Semantic Markup
- */
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
 
-/** Adds responsive viewport */
+// Add theme support for HTML5
+add_theme_support(
+	'html5',
+	array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption'
+	)
+);
+
+// Add responsive viewport markup
 add_theme_support( 'genesis-responsive-viewport' );
 
-/**
- * Include the rest of the theme files
- */
+// Add support for specified theme widgets
+add_theme_support(
+	'fortytwo-widgets',
+	array(
+		'ft-contact',
+		'ft-featured-content',
+		'ft-jumbotron',
+		'ft-responsive-slider',
+		'ft-tabs-widget',
+		'ft-testimonials',
+	)
+);
+
+// Include the rest of the theme files
 require_once 'lib/init.php';
--- theme/lib/init.php
+++ theme/lib/init.php
@@ -10,10 +10,9 @@
  * @link    http://forsitethemes/themes/fortytwo/
  */
 
-/* Child theme (do not remove) */
 define( 'CHILD_THEME_NAME', 'FortyTwo Theme' );
 define( 'CHILD_THEME_URL', 'http://forsitethemes.com/themes/fortytwo' );
-define( 'CHILD_THEME_VERSION', '1.0.0-RC4' );
+define( 'CHILD_THEME_VERSION', '0.8.0' );
 
 define( 'FORTYTWO_DIR', get_stylesheet_directory() );
 define( 'FORTYTWO_URL', get_stylesheet_directory_uri() );
@@ -24,7 +23,7 @@ define( 'FORTYTWO_LIB_URL', FORTYTWO_URL . '/lib' );
 define( 'FORTYTWO_WIDGETS_DIR', FORTYTWO_LIB_DIR . '/widgets' );
 define( 'FORTYTWO_WIDGETS_URL', FORTYTWO_LIB_URL . '/widgets' );
 
-/* Load Initializer Components */
+// Load components
 require_once 'admin/ft-admin-style.php';
 require_once 'admin/ft-layouts.php';
 require_once 'admin/ft-templates.php';
@@ -39,15 +38,29 @@ require_once 'structural/ft-nav.php';
 require_once 'structural/ft-subheader.php';
 require_once 'structural/ft-footer.php';
 
+add_action( 'widgets_init', 'ft_widgets_require', 5 );
+/**
+ * Require theme widget files.
+ *
+ * @since @@release
+ *
+ * @return null Return early if no theme widgets supported.
+ */
+function ft_widgets_require() {
+	if ( ! $widgets = get_theme_support( 'fortytwo-widgets' ) ) {
+		return;
+	}
 
-//Require all theme widgets
-require_once FORTYTWO_WIDGETS_DIR . '/ft-widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-responsive-slider/widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-featured-content/widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-jumbotron/widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-testimonials/widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-tabs-widget/widget.php';
-require_once FORTYTWO_WIDGETS_DIR . '/ft-contact/widget.php';
+	require_once FORTYTWO_WIDGETS_DIR . '/ft-widget.php';
 
-//Require once to include WPThumb
-require_once FORTYTWO_LIB_DIR . '/wpthumb/wpthumb.php';
+	foreach ( $widgets[0] as $widget ) {
+		$file = trailingslashit( FORTYTWO_WIDGETS_DIR ) . $widget . '/widget.php';
+		if ( is_readable( $file ) ) {
+			require_once $file;
+		}
+	}
+
+	if ( current_theme_supports( 'fortytwo-widgets', 'ft-responsive-slider' ) ) {
+		require_once FORTYTWO_LIB_DIR . '/wpthumb/wpthumb.php';
+	}
+}
