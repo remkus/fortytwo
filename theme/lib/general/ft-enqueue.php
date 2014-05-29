ccd8eb379b75d55a6295a3a866d424911f6ea4aa
Gary Jones <gary@garyjones.co.uk>
2014-05-29 08:16:30 +0100

    Add Open Sans 700-weight font
    normalize.css sets strong to be font-weight: 700. Since the
    heaviest we previously had was 600, the element wasn't actually
    appearing bold.
    
    Fixes #229

--- theme/lib/general/ft-enqueue.php
+++ theme/lib/general/ft-enqueue.php
@@ -15,7 +15,7 @@ add_action( 'wp_enqueue_scripts', 'fortytwo_enqueue_fonts' );
  * @since @@release
  */
 function fortytwo_enqueue_fonts() {
-	wp_enqueue_style( 'google-font-open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,600,300|Droid+Serif:400,700,400italic,700italic', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'google-font-open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,600,300,700|Droid+Serif:400,700,400italic,700italic', array(), CHILD_THEME_VERSION );
 }
 
 add_action( 'wp_enqueue_scripts', 'fortytwo_enqueue_scripts', 100 );
